f763efdec5dad77f77ea00ba467ebe6e5a0dc306
Alexandeiro <101557849+Alexandeiro@users.noreply.github.com>
2022-11-23 21:54:40 +0100

    Estructura de las escenas creada

---
+++ Fantasy-War---Leftovers-Studios-Fase-1/assets/Scripts/Inicio.js
@@ -0,0 +1,15 @@
+class Inicio extends Phaser.Scene {
+    constructor(){
+        super({key: "Inicio", active: true}); //key=nombre escena, active=se ve
+    }
+    preload(){}
+
+    create(){
+
+
+        //this.scene.start("GameScene");
+    }
+    
+    update(){}
+
+}
---
+++ Fantasy-War---Leftovers-Studios-Fase-1/assets/Scripts/Player.js
@@ -0,0 +1,18 @@
+class Player
+{
+    constructor(vida, oro, base, caminoSeleccionado)
+    {
+        this.vida = vida;
+        this.oro = oro;
+        this.base = base;
+        this.camino = caminoSeleccionado;
+        this.unidades = new Array();
+    }
+    AddUnidad(nuevaUnidad){
+        this.unidades.push(nuevaUnidad);
+        console.log(this.unidades);
+    }
+    RemoveUnidad(unidad){
+        this.unidades.remove(unidad);
+    }
+}
