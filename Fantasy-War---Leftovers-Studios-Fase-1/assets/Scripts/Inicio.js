08be3ac68d16cf187508cf83363218802b8c24a0
Alexandeiro <101557849+Alexandeiro@users.noreply.github.com>
2022-11-24 15:46:40 +0100

    Pantalla de inicio finalizada

--- Fantasy-War---Leftovers-Studios-Fase-1/assets/Scripts/Inicio.js
+++ Fantasy-War---Leftovers-Studios-Fase-1/assets/Scripts/Inicio.js
@@ -1,15 +1,33 @@
+
+
+var boton;
+
 class Inicio extends Phaser.Scene {
     constructor(){
         super({key: "Inicio", active: true}); //key=nombre escena, active=se ve
+       
     }
-    preload(){}
-
+    preload(){
+        this.load.image('inicio', 'assets/images/inicio.png');
+        this.load.image('boton', 'assets/images/boton.png');
+        
+    }
+    
     create(){
-
+        this.add.image(960  , 540, 'inicio');
+        boton= this.add.image(950, 820, 'boton').setInteractive();
+        boton.on('pointerdown', () =>{
+            this.scene.start('GameScene');  
+        });
 
         //this.scene.start("GameScene");
     }
     
-    update(){}
+    update(){
+        
+
+    }
+
 
 }
+
