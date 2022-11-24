428f22bd00d17100ad7aa7198e0f62462be495b6
Alexandeiro <101557849+Alexandeiro@users.noreply.github.com>
2022-11-24 16:18:32 +0100

    inicio con musicote

--- Fantasy-War---Leftovers-Studios-Fase-1/assets/Scripts/Inicio.js
+++ Fantasy-War---Leftovers-Studios-Fase-1/assets/Scripts/Inicio.js
@@ -10,11 +10,18 @@ class Inicio extends Phaser.Scene {
     preload(){
         this.load.image('inicio', 'assets/images/inicio.png');
         this.load.image('boton', 'assets/images/boton.png');
+        this.load.audio('musicote', 'assets/musica/musicote.mp3');
         
     }
     
     create(){
+        this.musica=this.sound.add('musicote');
+        this.musica.loop = true;
+        this.musica.volume = 0.1;
+        
+        
         this.add.image(960  , 540, 'inicio');
+        this.musica.play();
         boton= this.add.image(950, 820, 'boton').setInteractive();
         boton.on('pointerdown', () =>{
             this.scene.start('GameScene');  
