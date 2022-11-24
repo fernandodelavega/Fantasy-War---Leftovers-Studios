31382ee72ac7910b85c931f8c52cc3620f47fae1
fernandodelavega <115580903+fernandodelavega@users.noreply.github.com>
2022-11-24 15:40:23 +0100

    button y typedfs

---
+++ Fantasy-War---Leftovers-Studios-Fase-1/assets/Scripts/Button.js
@@ -0,0 +1,20 @@
+class Button
+{
+    constructor(coordX,coordY,nombre,scena,iPulsado,iNoPulsado){
+
+        this.coordX = coordX;
+        this.coordY = coordY;
+        this.nombre = nombre;
+        this.scena = scena
+        this.iPulsado = iPulsado;
+        this.iNoPulsado = iNoPulsado;
+    }
+
+    actionOnClick () {
+
+  
+    
+    }
+
+
+}
