0cb48d6d42a7c098d73fdaffa9db3f01e70ee900
dieguoin <dieguoin.developer@gmail.com>
2022-12-20 20:42:22 +0100

    starting char

--- FantasyWar/src/main/java/com/example/demo/ChatController.java
+++ FantasyWar/src/main/java/com/example/demo/ChatController.java
@@ -3,22 +3,35 @@ package com.example.demo;
 import java.io.File;
 import java.io.IOException;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/chat")
 public class ChatController {
 	
 	public static File msg;
 	public static TextToFile ttf;
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
 	public static void NewMessage(String text){
 		
 		ttf.NewText(msg, text);
 		
 	}
+	
+	@GetMapping
 	public static String[] GetMessage() throws IOException {
 		
 		return ttf.GetText(msg);
 	}
+	
+	
 	public static void main() throws IOException {
 		ttf = new TextToFile();
 		msg = new  File("./messageList");
---
+++ FantasyWar/src/main/resources/static/assets/Scripts/ChatPannel.js
@@ -0,0 +1,5 @@
+export class ChatPannel{
+    ChatPannel(image){
+        this.card = physics.add.image(0, 0, image);
+    }
+}
