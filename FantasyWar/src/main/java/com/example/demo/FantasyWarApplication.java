20ac9e732e6d4e6c7faf7e3e35b8221c3a861316
Alexandeiro <101557849+Alexandeiro@users.noreply.github.com>
2022-12-21 00:12:12 +0100

    cosas

--- FantasyWar/src/main/java/com/example/demo/FantasyWarApplication.java
+++ FantasyWar/src/main/java/com/example/demo/FantasyWarApplication.java
@@ -5,10 +5,16 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FantasyWarApplication {
 
+	
+	@Bean
+	public UserService usersService(){
+		return new UserService(10);
+	}
 	public static void main(String[] args){
 		
 		
---
+++ FantasyWar/src/main/java/com/example/demo/UserService.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+public class UserService {
+	
+	private int numUsers;
+	public UserService(int numUsers){
+	this.numUsers = numUsers;
+	}
+	public int getNumUsers() {
+	return numUsers;
+	}
+}
