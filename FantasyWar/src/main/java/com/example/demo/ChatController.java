80c1ed6e1775848dea87350dcacfc9b2a5c178d1
dieguoin <dieguoin.developer@gmail.com>
2022-12-21 01:05:08 +0100

    mensajes persistentes

--- FantasyWar/src/main/java/com/example/demo/ChatController.java
+++ FantasyWar/src/main/java/com/example/demo/ChatController.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,27 +15,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/chat")
 public class ChatController {
 	
-	public static File msg;
-	public static TextToFile ttf;
+	public static File msgs = new File("src\\main\\java\\com\\example\\demo\\messageList.txt");
+	public static TextToFile ttf = new TextToFile();
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public static void NewMessage(String text){
-		
-		ttf.NewText(msg, text);
+	public static void NewMessage(@RequestBody String text){
+
+		//sSystem.out.println(text + "abc");
+		ttf.NewText(msgs, text);
 		
 	}
 	
 	@GetMapping
 	public static String[] GetMessage() throws IOException {
 		
-		return ttf.GetText(msg);
+		return ttf.GetText(msgs);
 	}
 	
 	
 	public static void main() throws IOException {
-		ttf = new TextToFile();
-		msg = new  File("./messageList");
+
 		NewMessage("a");
 		String[] t = GetMessage();
 		for(String a :t) {
